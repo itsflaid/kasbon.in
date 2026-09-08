3ae7a2927efd29b2c8db40240cc358f6ea98ad4f
Mufacoderz <mf9555759@gmail.com>
2026-09-08 20:39:23 +0800

    feat(realtime): add EntryCreated broadcast event

---
+++ app/Events/EntryCreated.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Entry;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class EntryCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public Entry $entry) {}
+
+    public function broadcastOn(): array
+    {
+        return [new PrivateChannel('warung.'.$this->entry->warung_id)];
+    }
+}
